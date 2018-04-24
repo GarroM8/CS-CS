bd221adaa833a8ee3ae16d61403c7ef4625fbc31
GarroM8 <38668137+GarroM8@users.noreply.github.com>
2018-04-24 17:03:02 -0500

    Add files via upload

---
+++ steg.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+import sys
+import re
+import os
+
+a = 4
+
+if sys.argv[1] == '-B':
+    setByte = True
+    setBit = False
+elif sys.argv[1] == '-b':
+    setBit = True
+    setByte = False
+    interval = 1
+else:
+    print >> sys.stderr, "Byte or bit must be specified using -(bB)"
+
+if sys.argv[2] == '-s':
+    setStore = True
+    setRetrieve = False
+elif sys.argv[2] == '-r':
+    setRetrieve = True
+    setStore = False
+else:
+    print >> sys.stderr, "Store or Retrieve must be specified using -(sr)"
+
+if sys.argv[3].startswith('-o', 0, 2): 
+    offset = sys.argv[3]
+    offset = offset[2:]
+else:
+    print >> sys.stderr, "Offset must be specified using -o<val>"
+
+if sys.argv[4].startswith('-i', 0, 2): 
+    interval = sys.argv[4]
+    interval = interval[2:]
+    a=a+1
+elif sys.argv[1] == '-B':
+    print >> sys.stderr, "If in Byte, interval must be specified using -i<val>"
+
+if sys.argv[a].startswith('-w', 0, 2):
+    wfilename = sys.argv[a]
+    wfilename = wfilename[2:]
+    #wrapperfile = open(wfilename, mode = 'r', buffering = -1) #change buffering later
+else:
+    print >> sys.stderr, "Wrapper file must be specified using -w<val>"
+a=a+1
+
+if setStore is True:
+    if sys.argv[a].startswith('-h', 0, 2):
+        hfilename = sys.argv[a]
+        hfilename = hfilename[2:]
+        #hiddenfile = open(hfilename, mode = 'r', buffering = -1) #change buffering later
+    else:
+        print >> sys.stderr, "Hidden file must be specified using -h<val>"
+
+'''
+if setByte is True and setBit is False:
+    if setStore is True and setRetrieve is False:
+        #
+    elif setRetrieve is True and setStore is False:
+        #
+    else:
+        print >> sys.stderr, ""
+elif setBit is True and setByte is False:
+    if setStore is True and setRetrieve is False:
+        #
+    elif setRetrieve is True and setStore is False:
+        #
+    else:
+        print >> sys.stderr, ""
+'''
